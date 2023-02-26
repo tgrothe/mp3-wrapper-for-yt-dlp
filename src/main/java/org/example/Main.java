77f180622908f432ee147b858261abe30c555bd8
tgrothe <tgrothe@users.noreply.github.com>
2023-02-26 16:26:10 +0100

    Init 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,213 @@
+package org.example;
+
+import io.codeworth.panelmatic.PanelBuilder;
+import io.codeworth.panelmatic.PanelMatic;
+
+import javax.swing.*;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    private final Props props = new Props();
+    private final JTextField fieldSrc = new JTextField(props.properties.getProperty("fieldSrc"));
+    private final JTextField fieldDst = new JTextField(props.properties.getProperty("fieldDst"));
+    private final JTextField fieldReg = new JTextField(props.properties.getProperty("fieldReg"));
+    private final JTextArea fieldReg2 = new JTextArea(props.properties.getProperty("fieldReg2"));
+    private final JTextField fieldCmd = new JTextField(props.properties.getProperty("fieldCmd"));
+    private final JCheckBox boxRename = new JCheckBox("", Boolean.parseBoolean(props.properties.getProperty("boxRename")));
+    private final JCheckBox boxCopy = new JCheckBox("", Boolean.parseBoolean(props.properties.getProperty("boxCopy")));
+    private final JTextArea area = new JTextArea("");
+    private volatile boolean isRunning = false;
+    private Method renameMethod;
+
+    public Main() {
+        fieldReg2.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        area.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+
+        JButton button1 = new JButton("Start!");
+        JButton button2 = new JButton("Settings");
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(button1);
+        panel.add(button2);
+
+        JFrame frame = new JFrame("Download, rename, copy/sync");
+        frame.add(panel, BorderLayout.NORTH);
+        frame.add(new JScrollPane(area));
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                isRunning = false;
+            }
+        });
+        frame.setVisible(true);
+
+        button1.addActionListener(e -> {
+            if (isRunning) {
+                isRunning = false;
+                button1.setText("Start!");
+            } else {
+                isRunning = true;
+                button1.setText("Stop");
+                new Thread(this::startRun).start();
+            }
+        });
+        button2.addActionListener(e -> showSettings(frame));
+
+        showSettings(frame);
+    }
+
+    private void append(String s) throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            area.append(s + "\n");
+            area.setCaretPosition(area.getText().length());
+        });
+    }
+
+    @SuppressWarnings("all")
+    private void startRun() {
+        try {
+            while (isRunning) {
+                Pattern pat1 = Pattern.compile(fieldReg.getText());
+                String data = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+                if (data != null && data.matches(pat1.pattern())) {
+                    StringSelection dummyStr = new StringSelection("dummy text");
+                    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(dummyStr, dummyStr);
+                    Matcher mat1 = pat1.matcher(data);
+                    if (mat1.find()) {
+                        final String video_id = mat1.group(1);
+                        append("Found video id " + video_id);
+                        download(video_id);
+                        compileRenamer();
+                        renameFiles();
+                        copyFiles();
+                        append("Finish video id " + video_id);
+                    }
+                }
+
+                Thread.sleep(3000);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "An exception occurred!\n\n" + e, "Exception e", JOptionPane.WARNING_MESSAGE);
+            System.exit(0);
+        }
+    }
+
+    private void download(String video_id) throws Exception {
+        append("Start download " + video_id);
+        java.lang.Runtime rt = java.lang.Runtime.getRuntime();
+        @SuppressWarnings("all")
+        Process pr = rt.exec(String.format(fieldCmd.getText(), fieldSrc.getText(), video_id));
+        pr.waitFor();
+        append("Finish download " + video_id);
+    }
+
+    private void compileRenamer() throws Exception {
+        append("Start compile renamer");
+        // Prepare source somehow.
+        String source = fieldReg2.getText();
+        // Save source in .java file.
+        File root = Files.createTempDirectory("java").toFile();
+        File sourceFile = new File(root, "AdvancedRenamer.java");
+        append("mkdirs = " + sourceFile.getParentFile().mkdirs());
+        append("write = " + Files.writeString(sourceFile.toPath(), source));
+        // Compile source file.
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        append("compiler.run = " + compiler.run(null, null, null, sourceFile.getPath()));
+        // Load and instantiate compiled class.
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{root.toURI().toURL()});
+        Class<?> cls = Class.forName("AdvancedRenamer", true, classLoader);
+        renameMethod = cls.getDeclaredMethod("rename", String.class);
+        //  Object instance = cls.getDeclaredConstructor().newInstance();
+        append("Finish compile renamer");
+    }
+
+    private void renameFiles() throws Exception {
+        if (!boxRename.isSelected()) {
+            return;
+        }
+        append("Start renameFiles()");
+        File[] files = new File(fieldSrc.getText()).listFiles();
+        assert files != null;
+        for (File f : files) {
+            String fn = f.getName();
+            if (fn.endsWith(".mp3")) {
+                String prefix = fn.substring(0, fn.lastIndexOf('.'));
+                prefix = (String) renameMethod.invoke(null, prefix);
+                if (!new File(fieldSrc.getText() + prefix + ".mp3").exists()) {
+                    String a = f.getAbsolutePath();
+                    String b = fieldSrc.getText() + prefix + ".mp3";
+                    append(String.format("Rename %s to %s ... %b", a, b, new File(a).renameTo(new File(b))));
+                }
+            }
+        }
+        append("Finish renameFiles()");
+    }
+
+    private void copyFiles() throws Exception {
+        if (!new File(fieldDst.getText()).exists() || !boxCopy.isSelected()) {
+            return;
+        }
+        append("Start copyFiles()");
+        File[] files = new File(fieldSrc.getText()).listFiles();
+        assert files != null;
+        for (File f : files) {
+            String fn = f.getName();
+            if (fn.endsWith(".mp3")) {
+                String prefix = fn.substring(0, fn.lastIndexOf('.'));
+                prefix = (String) renameMethod.invoke(null, prefix);
+                if (!new File(fieldDst.getText() + prefix + ".mp3").exists()) {
+                    String a = f.getAbsolutePath();
+                    String b = fieldDst.getText() + prefix + ".mp3";
+                    append(String.format("Copy %s to %s ...", a, b));
+                    Files.copy(Path.of(a), Path.of(b), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+        append("Finish copyFiles()");
+    }
+
+    private void showSettings(JFrame frame) {
+        final JDialog dialog = new JDialog(frame, "Customization", true);
+        dialog.getContentPane().add(PanelMatic.begin()
+                .addHeader(PanelBuilder.HeaderLevel.H3, "Customization")
+                .add("Source folder:", fieldSrc)
+                .add("To copy folder:", fieldDst)
+                .add("RegEx (do not change):", fieldReg)
+                .add("Replace RegEx:", fieldReg2)
+                .add("CLI Command (do not change):", fieldCmd)
+                .add("Should rename?", boxRename)
+                .add("Should copy/sync?", boxCopy)
+                .get());
+        dialog.pack();
+        dialog.setSize(dialog.getWidth() + 50, dialog.getHeight());
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                fieldSrc.setText(new File(fieldSrc.getText()).getAbsolutePath() + File.separator);
+                fieldDst.setText(new File(fieldDst.getText()).getAbsolutePath() + File.separator);
+                props.storeProps(fieldSrc.getText(), fieldDst.getText(), fieldReg.getText(), fieldReg2.getText(), fieldCmd.getText(), boxRename.isSelected(), boxCopy.isSelected());
+            }
+        });
+        dialog.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
---
+++ src/main/java/org/example/Props.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+public class Props {
+    public final Properties properties = new Properties();
+    private final File pf = new File("MP3WatcherProps.txt");
+
+    public Props() {
+        try {
+            if (pf.exists()) {
+                properties.load(new FileInputStream(pf));
+            }
+            if (!properties.containsKey("fieldSrc")) {
+                properties.put("fieldSrc", "C:\\Users\\xxx\\Music\\yt-dlp\\");
+            }
+            if (!properties.containsKey("fieldDst")) {
+                properties.put("fieldDst", "D:\\yt-dlp\\");
+            }
+            if (!properties.containsKey("fieldReg")) {
+                properties.put("fieldReg", "^https://www\\.youtube\\.com/watch\\?v=([^?&]{10,12}).*$");
+            }
+            if (!properties.containsKey("fieldReg2")) {
+                properties.put("fieldReg2", """
+                        public class AdvancedRenamer {
+                            public static String rename(String prefix) {
+                                prefix = prefix.replaceAll("&", "n");
+                                prefix = prefix.replaceAll("_", " ");
+                                if (prefix.matches("^.+ \\\\[.{6,11}]$")) {
+                                    prefix = prefix.replaceAll(" \\\\[.{6,11}]$", "");
+                                } else if (prefix.matches("^.+ .{6,11} $")) {
+                                    prefix = prefix.replaceAll(" .{6,11} $", "");
+                                }
+                                prefix = prefix.replaceAll("[^-()\\\\wäöüß]+", " ");
+                                prefix = prefix.replaceAll(" {2,}", " ");
+                                prefix = prefix.trim().toLowerCase(java.util.Locale.ROOT);
+                                return prefix;
+                            }
+                        }"""
+                );
+            }
+            if (!properties.containsKey("fieldCmd")) {
+                properties.put("fieldCmd", "powershell.exe \"cd '%1$s' ; yt-dlp.exe -f bestaudio -x --audio-format mp3 --audio-quality 320K -- %2$s\"");
+            }
+            if (!properties.containsKey("boxRename")) {
+                properties.put("boxRename", "true");
+            }
+            if (!properties.containsKey("boxCopy")) {
+                properties.put("boxCopy", "true");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "An exception occurred!\n\n" + e, "Exception e", JOptionPane.WARNING_MESSAGE);
+            System.exit(0);
+        }
+    }
+
+    public void storeProps(String fieldSrc, String fieldDst, String fieldReg, String fieldReg2, String fieldCmd, boolean boxRename, boolean boxCopy) {
+        try {
+            properties.put("fieldSrc", fieldSrc);
+            properties.put("fieldDst", fieldDst);
+            properties.put("fieldReg", fieldReg);
+            properties.put("fieldReg2", fieldReg2);
+            properties.put("fieldCmd", fieldCmd);
+            properties.put("boxRename", String.valueOf(boxRename));
+            properties.put("boxCopy", String.valueOf(boxCopy));
+            try (FileOutputStream fos = new FileOutputStream(pf)) {
+                properties.store(fos, "Properties for MP3Watcher");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "An exception occurred!\n\n" + e, "Exception e", JOptionPane.WARNING_MESSAGE);
+            System.exit(0);
+        }
+    }
+}
